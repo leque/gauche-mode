f0090a37e8cf6f9f05181179050b891afe208954
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-06-16 01:27:43 +0900

    more tests

--- test/test.el
+++ test/test.el
@@ -1,16 +1,31 @@
+;;; -*- lexical-binding: t; -*-
 (require 'ert)
 (require 'gauche-mode)
 (require 'gauche-paredit)
 
 (add-hook 'gauche-mode-hook #'enable-gauche-paredit-mode)
 
+;; suppress `Matching: ...' output
+(setq blink-matching-paren nil)
+
 (defmacro gauche-with-temp-buffer (contents &rest body)
   (declare (indent 1))
-  `(with-temp-buffer
-     (insert ,contents)
-     (gauche-mode)
-     (font-lock-fontify-buffer)
-     ,@body))
+  `(save-window-excursion
+     (with-temp-buffer
+       (insert ,contents)
+       (pop-to-buffer (current-buffer))
+       (gauche-mode)
+       (font-lock-fontify-buffer)
+       ,@body)))
+
+(defun gauche-with-input-to-temp-buffer (macro)
+  (gauche-with-temp-buffer ""
+    (execute-kbd-macro macro)
+    (buffer-string)))
+
+(defun gauche-test-key-sequence (expected input)
+  (should (equal expected
+                 (gauche-with-input-to-temp-buffer input))))
 
 (defun gauche-test-string (str)
   (replace-regexp-in-string (rx bol (* space) "|")
@@ -74,6 +89,47 @@
 
 (ert-deftest gauche-paredit-slash-test ()
   (gauche-with-temp-buffer ""
-    (insert "#")
-    (gauche-paredit-slash)
-    (should (equal (buffer-string) "#//"))))
+    (execute-kbd-macro (kbd "#/"))
+    (should (equal (buffer-string) "#//"))
+    (font-lock-fontify-buffer)
+    (execute-kbd-macro (kbd "DEL"))
+    (should (equal (buffer-string) ""))
+    (execute-kbd-macro (kbd "#/a"))
+    (should (equal (buffer-string) "#/a/"))
+    (font-lock-fontify-buffer)
+    (backward-char)
+    (execute-kbd-macro (kbd "DEL"))
+    (should (equal (buffer-string) "#/a/"))))
+
+(ert-deftest gauche-paredit-datum-label-prefix-test ()
+  (dotimes (_ 100)
+    (let ((n (random 1000)))
+      (gauche-test-key-sequence (format "#%s=()" n)
+                                (kbd (format "# %s = (" n))))))
+
+(ert-deftest gauche-paredit-debug-print-prefix-test ()
+  (gauche-test-key-sequence "#?=()"
+                            (kbd "# ? = (")))
+
+(ert-deftest gauche-paredit-vector-prefix-test ()
+  (gauche-test-key-sequence "(f #vu8())"
+                            (kbd "(f SPC #vu8("))
+  (cl-loop for n in '(8 16 32 64)
+           for p in '(s u)
+           do (gauche-test-key-sequence (format "(f #%s%s())" p n)
+                                        (kbd (format "(f SPC #%s%s(" p n))))
+  (gauche-test-key-sequence "(f #s48 ())"
+                            (kbd "(f SPC #s48("))
+  (cl-loop for n in '(16 32 64)
+           do (gauche-test-key-sequence (format "(f #f%s())" n)
+                                        (kbd (format "(f SPC #f%s(" n))))
+  (gauche-test-key-sequence "(f #f8 ())"
+                            (kbd "(f SPC #f8("))
+  )
+
+(ert-deftest gauche-paredit-short-lambda-prefix-test ()
+  (gauche-test-key-sequence "(^(x) x)"
+                            (kbd "(^(x) SPC x"))
+  (gauche-test-key-sequence "(f (g) h)"
+                            (kbd "(f(g) SPC h"))
+  )
